adef4d941cab6f78eedd68ffdeb54705532330ce
陈佳乐 <hitwh__2015@163.com>
2017-09-22 21:19:39 +0800

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<!-- 连接数据库 -->
-<?php
-	$conn=mysqli_connect('localhost','root','123456','weibo');
-	if($conn->connect_error)
-	{
-		die("连接失败：".$conn->connect_error);
-	}
-	mysqli_query($conn,"set naems 'utf8'");
-?>
